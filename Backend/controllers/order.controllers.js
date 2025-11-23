9080d82da16cd6e67aa343c1535ef34620cb7ad2
Himanshu <himanshugupta8582@gmail.com>
2025-11-23 14:36:05 +0530

    Backend Completed

--- Backend/controllers/order.controllers.js
+++ Backend/controllers/order.controllers.js
@@ -2,21 +2,21 @@ import Cart from "../models/cart.models.js";
 import Order from "../models/order.models.js";
 
 export const createOrder = async (req, res) => {
-    const cart = await Cart.findOne({ userId: req.user._id });
+    const cart = await Cart.findOne({ userId: req.userId });
     if (!cart) return res.status(400).json({ error: "Cart empty" });
 
     const order = await Order.create({
-        userId: req.user._id,
+        userId: req.userId,
         items: cart.items
     });
 
-    await Cart.deleteOne({ userId: req.user._id });
+    await Cart.deleteOne({ userId: req.userId });
 
     res.json({ message: "Order successful", order });
 };
 
 export const listOrders = async (req, res) => {
-    const orders = await Order.find({ userId: req.user._id })
+    const orders = await Order.find({ userId: req.userId })
         .populate("items.itemId", "name price");
     res.json(orders);
 };
